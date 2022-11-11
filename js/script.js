a759bb244e2803edabd2496739f860f7c5a9abe3
Denis <tonopik@gmail.com>
2022-11-11 14:59:08 +0200

    babel, polifills

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
 import tabs from "./modules/tabs";
 import modals from "./modules/modals";
 import timer from "./modules/timer";
